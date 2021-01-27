39f1905fa2e1222d44ce6163702a4d975267d95f
saraytin <77133961+saraytin@users.noreply.github.com>
2021-01-26 22:37:18 -0600

    Edit from Sam

--- src/Parker/Main.java
+++ src/Parker/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 
 	    System.out.println("Hello, my name is Hannah.");
+	    System.out.println("Hello Hannah, my name is Sam.");
     }
 }
